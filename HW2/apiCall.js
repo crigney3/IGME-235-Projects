12d2e5cfe00bd25e2fb8bb90d3a6601cf8626917
Corey Rigney <coreyrigney@gmail.com>
2019-11-15 15:16:18 -0500

    Basic Proposal complete - HW2

---
+++ HW2/apiCall.js
@@ -0,0 +1,107 @@
+// 1
+window.onload = (e) => {//document.querySelector("#search").onclick = searchButtonClicked;
+getSet();};
+	
+// 2
+let displayTerm = "";
+
+// 3
+function searchButtonClicked(){
+    console.log("searchButtonClicked() called");
+    
+    const BLIZZARD_URL = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/";
+    //const BLIZZARD_KEY = "fa9fb7142dmsh9fdc1344c4d46c5p11b649jsn97699a6a8d0f";
+
+    let url = BLIZZARD_URL;
+    //url += "api_key=" + BLIZZARD_KEY;
+
+    let term = document.querySelector("#searchterm").value;
+    displayTerm = term;
+
+    term = term.trim();
+
+    term = encodeURIComponent(term);
+
+    if(term.length < 1) return;
+
+    url += "&q=" + term;
+
+    let limit = document.querySelector("#limit").value;
+    url += "&limit=" + limit;
+
+    document.querySelector("#status").innerHTML = "<b>Searching for '" + displayTerm + "'</b>";
+
+    console.log(url);
+
+    getData(url);
+}
+
+function getSet(set="Ysera"){
+    set = set.trim();
+
+    set = encodeURIComponent(set);
+
+    if(set.length < 1) return;
+
+    const BLIZZARD_URL = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + set;
+
+    getData(BLIZZARD_URL);
+}
+
+function getData(url){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = dataLoaded;
+
+    xhr.onerror = dataError;
+
+    xhr.open("GET", url);
+    xhr.setRequestHeader("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "fa9fb7142dmsh9fdc1344c4d46c5p11b649jsn97699a6a8d0f");
+    xhr.send();
+}
+
+function dataLoaded(e){
+    let xhr = e.target;
+
+    console.log(xhr.responseText);
+
+    let obj = JSON.parse(xhr.responseText);
+
+    //if(!obj.data || obj.data.length == 0){
+        //document.querySelector("#status").innerHTML = "<b>No results found </b>";
+        //return;
+    //}
+
+    let results = obj;
+
+    console.log(results);
+    //console.log("results.length = " + results.length);
+    //let bigString = "<p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
+
+    /*for(let i=0; i<results.length;i++){
+        let result = results[i];
+
+        if(result.collectible != true) return false;
+
+        let smallURL = result.images.fixed_width_small.url;
+        if(!smallURL) smallURL = "images/no-image-found.png";
+
+        let url = result.url;
+
+        let line = `<div class='result'><img src='${smallURL}' title='${result.id}' />`;
+        line += `<span></span></div>`;
+
+        bigString += line;
+    }*/
+
+    bigString = `<div class='image'><img src='${results[0].imgGold}' title='${results[0].name}' />`;
+
+    document.querySelector("#content").innerHTML = bigString;
+
+    document.querySelector("#status").innerHTML = "<b>Success!</b>";
+}
+
+function dataError(e){
+    console.log("An error occurred");
+}
